42cbdb9cbf525a29316f9162da373619206456c5
melpon <melt@e-ml.jp>
2014-01-26 00:36:21 +0900

    add tmux settings and alingta vim plugin

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 cd `dirname $0`
 
-for file in `ls -A | grep -v 'setup.sh$' | grep -v '.git$' | grep -v '.gitmodules$'`; do
+for file in `ls -A | grep -v 'setup.sh$' | grep -v '.git$' | grep -v '.gitignore$' | grep -v '.gitmodules$'`; do
+    if [ -h $HOME/$file ]; then
+        rm $HOME/$file
+    fi
     ln -s $PWD/$file $HOME/$file
 done
