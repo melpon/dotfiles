94c75f62dc81fec846e3ebe4644b1201e36d8911
melpon <shigemasa7watanabe+github@gmail.com>
2022-04-10 04:40:13 +0900

    MinGW 起動時に Visual Studio のパスを通す

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ cd `dirname $0`
 git submodule update -i --recursive
 
 for file in `ls -A | grep -v 'setup.sh$' | grep -v '.git$' | grep -v '.gitignore$' | grep -v '.gitmodules$'`; do
-    if [ -h $HOME/$file ]; then
-        rm $HOME/$file
+    if [ -z "$file" ]; then
+      continue
+    fi
+    if [ -e $HOME/$file ]; then
+        rm -rf $HOME/$file
     fi
     ln -s $PWD/$file $HOME/$file
 done
